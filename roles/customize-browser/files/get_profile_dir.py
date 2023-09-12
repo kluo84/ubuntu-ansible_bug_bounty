e3e8ab0ab3abbc920c3e8123b6b3c7eaade6e2b5
kluo84 <15222944+kluo84@users.noreply.github.com>
2023-09-12 00:24:50 -0500

    add os

--- roles/customize-browser/files/get_profile_dir.py
+++ roles/customize-browser/files/get_profile_dir.py
@@ -1,19 +1,20 @@
+import os
 import configparser
-import sys
 
 def get_profile_dir(profile_name):
     config = configparser.ConfigParser()
     config.read(os.path.expanduser('~/.mozilla/firefox/profiles.ini'))
     
     for section in config.sections():
-        if config.has_option(section, 'Name') and config[section]['Name'] == profile_name:
-            return config[section]['Path']
+        if config.has_option(section, 'Name') and config.get(section, 'Name') == profile_name:
+            return config.get(section, 'Path')
     return None
 
 if __name__ == "__main__":
+    import sys
     profile_name = sys.argv[1]
     profile_dir = get_profile_dir(profile_name)
     if profile_dir:
         print(profile_dir)
     else:
-        print(f"No directory found for profile: {profile_name}")
+        sys.exit(1)
